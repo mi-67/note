97581e3dccd30fd9d0eb51812947309ca2bd59d6
Miwa Matsubara <e165414k@st.u-gakugei.ac.jp>
2022-12-12 22:36:15 +0900

    5章終わりまで

---
+++ ts/TypeScriptBeginner/src/section_5-6-3.ts
@@ -0,0 +1,8 @@
+function createUser (name: string, age: number) {
+  return (message: string) => {
+    return `${name}(${age})「${message}」`
+  }
+}
+
+const getMessage = createUser('uhyo', 26)
+console.log(getMessage('こんにちは'))
